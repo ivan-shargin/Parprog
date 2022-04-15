7887c3500e6f0b7cff45a87cb280aad5fdf6c269
ivan-shargin <Shargin.IA@phystech.edu>
2022-04-15 15:01:57 +0300

    tried gather - in a wrong way

--- lab1/lab1_mpi.c
+++ lab1/lab1_mpi.c
@@ -6,22 +6,22 @@
 
 const int M = 10000;
 const int K = 1000;
+const double x0 = 0, xM = 1;
+const double a = 0.5;
+const double T = 1;
+const int D_k = 100;
 
 int main (int argc,char **argv)
 {
-    double x0 = 0, xM = 1;
     double U0[M];
-    double a = 0.5;
     double h = (xM - x0) / (M - 1);
-    double T = 1;
     double tau = T / (K - 1);
     double C = a * tau / h;
-    int j = 0, m = 0, k = 0;
+    int j = 0, i = 0, m = 0, k = 0;
     int len = M * K;
     int min_m = 0, max_m = M, width = M;
+    int size = 0, rank = 0;
 
-    int size = 0;
-    int rank = 0;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -43,15 +43,15 @@ int main (int argc,char **argv)
     }
 
     for (m = 0;m <= 100; m++){
-        U0[m] = -(m - 100) * (m -100) / 100 + 100;
+        U0[m] = -(m - 100) * (m -100) / 100;
     }
 
-    double *f = malloc(sizeof(double) * width * K);
+    double *f = (double *) malloc(sizeof(double) * width * K);
     for (j = 0;j < width * K; j++){
         f[j] = 0;
     }
 
-    double *Solution = malloc(sizeof(double) * width * K);
+    double *Solution = (double*) malloc(width * K * sizeof(double));
     double t1 = MPI_Wtime();
     computing_cycle(size, rank, f, &U0[min_m], Solution, C, tau, K, M);
     double t2 = MPI_Wtime();
@@ -59,25 +59,32 @@ int main (int argc,char **argv)
     double time = t2 - t1;
     printf("For rank = %d computing took %f\n", rank, time);
 
-    // if (rank == 0){
-    //     FILE *file = NULL;
-    //     file = fopen("solution.txt", "wb");
-    //     if (file == NULL){
-    //         printf("Can't open the output file!");
-    //         getchar();
-    //         return -1;
-    //     }
+    int root = 0;
+    if (rank == root){
+        double *output = (double*) malloc(len * sizeof(double));
+        MPI_Gather(Solution, width * K, MPI_DOUBLE, output, width * K, MPI_DOUBLE, root, MPI_COMM_WORLD);
 
-    //     for (k = 0; k < K; k += 10){
-    //         for (m = 0; m <= M; m ++){
-    //             j = ind(k, m, K, M);
-    //             fprintf(file, "%f", Solution[j]);
-    //             fprintf(file, "%c", ',');
-    //         }
-    //     }
-    //     fclose(file);
-    //     free(Solution);
-    // }
+        FILE *file = NULL;
+        file = fopen("output.bin", "wb");
+        if (file == NULL){
+            printf("Can't open the output file!");
+            getchar();
+            return -1;
+        }
+
+        for(k = 0;k < K;k += D_k)
+            for (i = k * M ;i < (k+1) * M; i++){
+                fprintf(file, "%f", output[i]);
+            }
+
+        fclose(file);
+        free(output);
+    }else{
+        MPI_Gather(Solution, width * K, MPI_DOUBLE, NULL, width * K, MPI_DOUBLE, root, MPI_COMM_WORLD);
+    }
+
+    free(Solution);
+    free(f);
     MPI_Finalize();
     return 0;
 }
